99a49af3c9b169862cf6263bc47c7272c3928e42
Priyanka <babarpriyanka323@gmail.com>
2023-05-03 14:01:23 +0530

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require ('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const doctorsRouters = require('./API/router/doctors')
@@ -7,12 +8,16 @@ const centresRouters = require('./API/router/centres')
 const appointmentsRouter = require('./API/router/appointments')
 const usersRouter = require('./API/router/users')
 const mongoose = require("mongoose")
-
 require('dotenv').config
 mongoose.connect(process.env.mongoConnection)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({ extended: true }))
+app.use(
+    cors({
+        origin:"*"
+    })
+)
 app.use(morgan("dev"))
 
 app.use('/doctors', doctorsRouters)
